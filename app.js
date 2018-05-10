60daa63093d4c4a507f02db64e0864aa1db3b555
Zabai Armas Herrera <zbrmas@hotmail.com>
2018-05-10 21:34:33 +0100

    View json messages of user
    Changed routes order to solve conflicts => accessing to /home/user/messages first goes through /home/user/:id

--- app.js
+++ app.js
@@ -61,9 +61,9 @@ app.use(function isLogged(req, res, next) {
     else res.redirect('/');
 });
 app.use('/home', require('./routes/home'));
-app.use('/home/users', require('./routes/users'));
-app.use('/home/users/friends', require('./routes/friends'));
 app.use('/home/users/messages', require('./routes/messages'));
+app.use('/home/users/friends', require('./routes/friends'));
+app.use('/home/users', require('./routes/users'));
 app.use('/home/group', require('./routes/group'));
 app.use('/home/posts', require('./routes/posts'));
 
--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,35 @@
 var express = require("express");
 var router = express.Router();
 
+router.get('/', function(req, res, next) {
+    var client = require('../db/db');
+
+    client.query("SELECT messages.id, profiles.name, profiles.surname, content \n" +
+        "FROM messages\n" +
+        "INNER JOIN profiles ON profiles.id=sender\n" +
+        "WHERE receiver=:user " +
+        "ORDER BY id ASC",
+        {user: req.user.id},
+        function(err, messagesRows) {
+            if(err) res.status(500).send('Error. La hemos cagado en algo...');
+            var messages = {};
+            messagesRows.forEach(function(message) {
+                if(!messages[message.name])
+                    messages[message.name] = [];
+                messages[message.name].push(message);
+            });
+            res.send(messages);
+        });
+
+    client.end();
+});
+
 router.post("/send", function(req, res, next){
     var messageData = {
         from: req.body.from,
         to: req.body.to,
         content: req.body.content
     };
-    console.log('Mensaje Privado: ', messageData);
     var dbConn = require("../db/db");
     dbConn.query("INSERT INTO messages (sender, receiver, content) VALUES((SELECT profiles.user_id FROM profiles WHERE profiles.email=:from), (SELECT profiles.user_id FROM profiles WHERE profiles.email=:to), :content)",
         {from: messageData.from, to: messageData.to, content: messageData.content},
